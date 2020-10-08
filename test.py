2c6806735cc298f99532a550955d54f4dab79175
thomaskost17 <thomaskost17@ucla.edu>
2020-10-08 01:01:52 -0700

    test python script after opencv install

---
+++ test.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    x = "ECE_180_DA_DB"
+    if x == "EE_180_DA_DB":
+        print("You are living in 2017")
+    else:
+        # this is a comment
+        x = x + " - Best class ever"
+        print(x)
+    
