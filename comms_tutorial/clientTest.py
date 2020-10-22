a9caa16ac0ee421df5906073ff1953eef80528ce
thomaskost17 <thomaskost17@ucla.edu>
2020-10-22 02:28:58 -0700

    fixing typo

--- comms_tutorial/clientTest.py
+++ comms_tutorial/clientTest.py
@@ -1,5 +1,5 @@
 import socket
-client = socket.socket(socet.AF_INET, socket.SOCK_STREAM)   
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
 client.connect('0.0.0.0', 8080)
 client.send("I am CLIENT\n")
 from_server = client.recv(4096)
