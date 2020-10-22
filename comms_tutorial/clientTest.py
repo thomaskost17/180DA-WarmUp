74801bfaedcd04476e256653f910a556e13fdd3f
thomaskost17 <thomaskost17@ucla.edu>
2020-10-22 02:27:30 -0700

    comms files

---
+++ comms_tutorial/clientTest.py
@@ -0,0 +1,7 @@
+import socket
+client = socket.socket(socet.AF_INET, socket.SOCK_STREAM)   
+client.connect('0.0.0.0', 8080)
+client.send("I am CLIENT\n")
+from_server = client.recv(4096)
+client.close()
+print(from_server)
