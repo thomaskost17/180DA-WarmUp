db975f0d0836f19927adda85a66a2fb03b64dfaa
thomaskost17 <thomaskost17@ucla.edu>
2020-10-22 02:34:38 -0700

    typos part 2

--- comms_tutorial/clientTest.py
+++ comms_tutorial/clientTest.py
@@ -1,6 +1,6 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
-client.connect('0.0.0.0', 8080)
+client.connect(('0.0.0.0', 8081))
 client.send("I am CLIENT\n")
 from_server = client.recv(4096)
 client.close()
--- comms_tutorial/serverTest.py
+++ comms_tutorial/serverTest.py
@@ -2,7 +2,7 @@ import socket
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Assigns a port for server that listens to clients connecting to this port
-serv.bind('0.0.0.0', 8080)
+serv.bind(('0.0.0.0', 8081))
 serv.listen(5)
 while True:
     conn, addr = serv.accept()
