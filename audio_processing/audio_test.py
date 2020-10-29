03bf14c4db253b331a171a74dd6081a4fc2d9a22
thomaskost17 <thomaskost17@ucla.edu>
2020-10-29 15:19:36 -0700

    audio processing code--using sphynx

---
+++ audio_processing/audio_test.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
